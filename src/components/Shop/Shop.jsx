b295dad468865ad860c746c8d862bd7b86f0edd4
almahmudsarker <almahmuds427@gmail.com>
2023-03-29 02:33:38 +0600

    added shopping cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,21 +4,33 @@ import { useEffect, useState } from "react";
 import Product from "../Product/Product";
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch("./products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
+  const handleAddToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
+
   return (
     <div className="shop-container">
       <div className="products-container">
         {products.map((product) => (
-          <Product key={product.id} product={product}></Product>
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Product>
         ))}
       </div>
       <div className="cart-container">
         <h4>Order summery</h4>
+        <p>Selected Item: {cart.length}</p>
       </div>
     </div>
   );
