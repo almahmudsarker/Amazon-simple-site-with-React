116ccbce67cd83179a9b05cfb7b4b3babaddb202
almahmudsarker <almahmuds427@gmail.com>
2023-03-29 01:48:43 +0600

    added products

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Shop.css";
+import { useEffect, useState } from "react";
+import Product from "../Product/Product";
+const Shop = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("./products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        {products.map((product) => (
+          <Product key={product.id} product={product}></Product>
+        ))}
+      </div>
+      <div className="cart-container">
+        <h4>Order summery</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
