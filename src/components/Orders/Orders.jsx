8e981fce1cb74a6d9ba7da5da3338fa37cbe169c
almahmudsarker <almahmuds427@gmail.com>
2023-04-06 01:14:16 +0600

    remove item from review

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Orders.css';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
-    const cart = useLoaderData();
-    
+    const savedCart = useLoaderData();
+    const [cart, setCart] = useState(savedCart);
+
+    const handleRemoveFromCart = (id) => {
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
     return (
         <div className='shop-container'>
-            <div className='products-container'>
-                <h1>Order Page: {cart.length}</h1>
+            <div className='review-container'>
+                {
+                    cart.map(product => <ReviewItem 
+                    key={product.id}
+                    product={product}
+                    handleRemoveFromCart={handleRemoveFromCart}
+                    ></ReviewItem>)
+                }
             </div>
             <div className='cart-container'>
                 <Cart cart={cart}></Cart>
